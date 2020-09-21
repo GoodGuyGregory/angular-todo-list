486c5410b480159b942ef24401d2c1d879cf93de
GoodGuyGregory <greg.witt625@gmail.com>
2020-09-21 11:58:07 -0400

    adds working project for demo

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // title = 'angular-crash-todolist';
+
+  // name: string = 'Greg';
+
+  // //  runs when the component is created Oninit
+  // constructor() {
+  //   this.changeName('Jon');
+  // }
+
+  // changeName(name: string) {
+  //     this.name = name;
+  // }
+
+  
+}
